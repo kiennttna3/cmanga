ca2c7674180f38783fb76b716194ea7efc8008b9
kiennttna3 <kiennttna3@gmail.com>
2024-04-02 11:33:41 +0700

    update v12.2

--- resources/views/pages/home/listGroup.blade.php
+++ resources/views/pages/home/listGroup.blade.php
@@ -74,7 +74,6 @@
             max-width: 100%;
             flex: 0 0 100%;
             margin-bottom: auto;
-            height: 160px;
         }
         .grid_list .hot_tag {
             display: block;
@@ -104,14 +103,13 @@
         }
         .grid_list .product__item__text {
             padding: 0;
-            width: 86%;
+            width: -webkit-fill-available;
             height: -webkit-fill-available;
         }
         .grid_list .product__item__lastchapter {
+            position: static;
             width: -webkit-fill-available;
-            padding-left: calc(100% - 86%);
-            padding-right: calc(100% - 97%);
-            bottom: 28px;
+            padding: 15px 0 0 0;
         }
         .grid_list .hot_tags {
             display: none;
@@ -127,16 +125,12 @@
             display: none;
         }
         .grid_list .ep1 {
-            font-size: 16px;
+            font-size: 14px;
             color: #ffffff;
         }
         .grid_list .product__item__option {
-            position: absolute;
             display: flex;
             width: -webkit-fill-available;
-            padding-left: calc(100% - 85%);
-            padding-right: calc(100% - 97%);
-            bottom: 70px;
         }
         .grid_list .product__item .option_follow,
         .grid_list .product__item .option_view {
@@ -147,10 +141,7 @@
             color: #6C74FC;
         }
         .grid_list .product__item__text h6 {
-            max-width: calc(100%);
-            width: 100%;
-            max-height: 42px;
-            overflow: hidden;
+            margin-bottom: 10px;
         }
         .grid_list .product__item__text h6 a {
             font-size: 16px;
@@ -159,11 +150,13 @@
         .grid_list .product__item .background_option1 {
             padding: 24px 0 0 0;
         }
+        .background_size_bar {
+            width: -webkit-fill-available;
+        }
         @media only screen and (min-width: 992px) and (max-width: 1199px) {
             .grid_list .product__item__lastchapter,
             .grid_list .product__item__option {
-                padding-left: calc(100% - 84%);
-                padding-right: calc(100% - 97%);
+                padding-left: calc(100% - 100%);
             }
             .product__item .ep {
                 font-size: 15px;
@@ -179,18 +172,10 @@
                 height: 290px;
             }
         }
-        @media only screen and (min-width: 768px) and (max-width: 768px) {
-            .grid_list .product__item__lastchapter,
-            .grid_list .product__item__option {
-                padding-left: calc(100% - 85%) !important;
-                padding-right: calc(100% - 97%) !important;
-            }
-        }
         @media only screen and (min-width: 576px) and (max-width: 768px) {
             .grid_list .product__item__lastchapter,
             .grid_list .product__item__option {
-                padding-left: calc(100% - 81%);
-                padding-right: calc(100% - 96%);
+                padding-left: calc(100% - 100%);
             }
             .product__item .ep {
                 font-size: 15px;
@@ -200,8 +185,7 @@
         @media only screen and (min-width: 425px) and (max-width: 576px) {
             .grid_list .product__item__lastchapter,
             .grid_list .product__item__option {
-                padding-left: 100px;
-                padding-right: calc(100% - 95%);
+                padding-left: calc(100% - 100%);
             }
             .grid_list .product__item .background_option1 {
                 padding: 22px 0 0 0;
@@ -218,9 +202,9 @@
         @media only screen and (min-width: 375px) and (max-width: 425px) {
             .grid_list .product__item__lastchapter,
             .grid_list .product__item__option {
-                padding-left: 95px;
-                padding-right: calc(100% - 94%);
+                padding-left: calc(100% - 100%);
             }
+
             .grid_list .product__item .background_option1 {
                 padding: 22px 0 0 0;
             }
@@ -228,8 +212,7 @@
         @media only screen and (min-width: 320px) and (max-width: 375px) {
             .grid_list .product__item__lastchapter,
             .grid_list .product__item__option {
-                padding-left: 90px;
-                padding-right: calc(100% - 94%);
+                padding-left: calc(100% - 100%);
             }
             .grid_list .product__item .background_option1 {
                 padding: 22px 0 0 0;
@@ -238,8 +221,7 @@
         @media only screen and (max-width: 320px) {
             .grid_list .product__item__lastchapter,
             .grid_list .product__item__option {
-                padding-left: 86px;
-                padding-right: calc(100% - 94%);
+                padding-left: calc(100% - 100%);
             }
             .grid_list .product__item .background_option1 {
                 padding: 16px 0 0 0;
@@ -262,7 +244,7 @@
 @foreach ($bookstory as $key => $value)
     <div class="list_Group product__item col-xl-3 col-lg-4 col-md-4 col-sm-4 col-6">
         <div class="product__item">
-            <div class="product__item__pic set-bg" data-setbg="{{ Voyager::image($value->image) }}" style="background-image: url('{{ Voyager::image($value->image) }}');">
+            <div class="product__item__pic set-bg" data-setbg="{{ Voyager::image($value->image) }}">
                 <a title="{{ $value->title }}" href="{{ route('bookstory', [$value->slug_book]) }}"><img class="product__item__pic"></a>
                 @if ($value->featured)
                     <img class="hot_tags" src="{{ asset('img/hot_tags.png') }}">
@@ -289,58 +271,60 @@
                     </div>
                 </div>
             </div>
-            <div class="product__item__text text_capitalize">
-                <h6>
-                    <a title="{{ $value->title }}" href="{{ route('bookstory', [$value->slug_book]) }}">
-                        {{ $value->title }}
-                    </a>
-                </h6>
-                @if ($value->featured)
-                    <img class="hot_tag" src="{{ asset('img/hot_tags.png') }}">
-                @endif
-            </div>
-            <div class="product__item__option justify-content-between align-items-center">
-                <div class="btn-group ep1">
-                    @if ($check)
-                        @if ($value->title_name)
-                            <a title="{{ $value->title }} {{ $value->title_name }}" href="{{ route('chapter', [$value->slug_book, $value->slug]) }}">
-                                <i class="fa-solid fa-location-dot"></i>
-                                <div class="title_name">
-                                    Đang đọc {{ $value->title_name }}
-                                </div>
-                            </a>
+            <div class="background_size_bar">
+                <div class="product__item__text text_capitalize">
+                    <h6>
+                        <a title="{{ $value->title }}" href="{{ route('bookstory', [$value->slug_book]) }}">
+                            {{ $value->title }}
+                            @if ($value->featured)
+                                <img class="hot_tag" src="{{ asset('img/hot_tags.png') }}">
+                            @endif
+                        </a>
+                    </h6>
+                </div>
+                <div class="product__item__option justify-content-between align-items-center">
+                    <div class="btn-group ep1">
+                        @if ($check)
+                            @if ($value->title_name)
+                                <a title="{{ $value->title }} {{ $value->title_name }}" href="{{ route('chapter', [$value->slug_book, $value->slug]) }}">
+                                    <i class="fa-solid fa-location-dot"></i>
+                                    <div class="title_name">
+                                        Đang đọc {{ $value->title_name }}
+                                    </div>
+                                </a>
+                            @endif
                         @endif
-                    @endif
+                    </div>
+                    <div class="btn-group background_option1">
+                        <div class="option_view">
+                            <i class="fa fa-eye"></i>
+                            {{ formatNumber($value->view) }}
+                        </div>
+                        <div class="option_follow">
+                            <i class="fa fa-bookmark"></i>
+                            {{ formatNumber($value->follow) }}
+                        </div>
+                    </div>
                 </div>
-                <div class="btn-group background_option1">
-                    <div class="option_view">
-                        <i class="fa fa-eye"></i>
-                        {{ formatNumber($value->view) }}
-                    </div>
-                    <div class="option_follow">
-                        <i class="fa fa-bookmark"></i>
-                        {{ formatNumber($value->follow) }}
-                    </div>
-                </div>
-            </div>
-            @if ($value->chapter_title)
-                <div class="justify-content-between align-items-center mt-3 product__item__lastchapter">
-                    <div class="btn-group">
-                        <small class="text-muted">
-                            <a title="{{ $value->title_name }} {{ $value->chapter_title }}" href="{{ route('chapter', [$value->slug_book, $value->chapter_slug]) }}">
-                                {{ $value->chapter_title }}
-                            </a>
-                        </small>
-                    </div>
-                    @if ($value->chapter_created_at)
+                @if ($value->chapter_title)
+                    <div class="justify-content-between align-items-center mt-3 product__item__lastchapter">
                         <div class="btn-group">
-                            <small class="text-muted" data-time="{{ $value->created_at }}">
-                                {{ \Carbon\Carbon::parse($value->chapter_created_at)->diffForHumans() }}
+                            <small class="text-muted">
+                                <a title="{{ $value->title_name }} {{ $value->chapter_title }}" href="{{ route('chapter', [$value->slug_book, $value->chapter_slug]) }}">
+                                    {{ $value->chapter_title }}
+                                </a>
                             </small>
                         </div>
-                    @endif
-                </div>
-            @endif
+                        @if ($value->chapter_created_at)
+                            <div class="btn-group">
+                                <small class="text-muted" data-time="{{ $value->created_at }}">
+                                    {{ \Carbon\Carbon::parse($value->chapter_created_at)->diffForHumans() }}
+                                </small>
+                            </div>
+                        @endif
+                    </div>
+                @endif
+            </div>
             <div class="clear"></div>
         </div>
     </div>
